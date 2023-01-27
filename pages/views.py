4429518952881a2030da3c398c0238e660837f86
Owen Ndlhovu <owen.ndlhovu@tshiamisotrust.com>
2023-01-27 11:25:29 +0200

    create pages

--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request ,'pages/home.html')
+
+def about(request):
+    return render(request ,'pages/about.html')
+
+def services(request):
+    return render(request ,'pages/services.html')
+
+def contact(request):
+    return render(request ,'pages/contact.html')
+
+def login(request):
+    return render(request ,'pages/login.html')
+
+def signup(request):
+    return render (request ,'pages/signup.html')
